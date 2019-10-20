580f0649c0a62c85514c2a77a0006e7901c17086
Kayley Bannister <kayley.bannister@gmail.com>
2019-10-20 14:54:22 -0400

    added JSX into Contact.js file but React is being funky, will keep trying to get it fixed

--- oldmariettalandscaping/client/src/App.js
+++ oldmariettalandscaping/client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Contact from "../src/components/Contact";
 import Carousel from "../src/components/Carousel";
+import Contact from "../src/components/Contact";
+
 
 class App extends Component {
   render() {
@@ -15,15 +16,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
-      
-      //Kayley adding the divs for the Carousel and Contact components
+        
       <div>
         <Carousel />
       </div>
       <div>
         <Component />
       </div>
+      </div>
+      
     )
   }
 }
--- oldmariettalandscaping/client/src/components/Contact.js
+++ oldmariettalandscaping/client/src/components/Contact.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 
-function() {
+class App extends Component {
 render() {
     return(
-    <div id="contact" className="text-center">
-  <div className="container">
-    <div class="section-title text-center">
-      <h2>Contact Us</h2>
-      <hr>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
-    </div>
-    <div className="col-md-10 col-md-offset-1 contact-info">
-      <div className="col-md-4">
-        <h3>Address</h3>
-        <hr>
-        <div class="contact-item">
-          <p>Address</p>
-        </div>
-      </div>
+        <div id="contact" className="text-center">
+          <div className="container">
+            <div className="section-title text-center">
+              <h2>Contact Us</h2>
+              <hr/>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.</p>
+            </div>
+            <div className="col-md-10 col-md-offset-1 contact-info">
+              <div className="col-md-4">
+                <h3>Address</h3>
+                <hr/>
+                <div className="contact-item">
+                <p>Address</p>
+              </div>
+            </div>
 
       <div className="col-md-4">
         <h3>Working Hours</h3>
-        <hr>
+        <hr />
         <div className="contact-item">
           <p>Monday-Saturday: 6 AM - 5 PM</p>
           <p>Sunday: 11 AM - 12 PM</p>
@@ -29,12 +29,11 @@ render() {
       </div>
       <div className="col-md-4">
         <h3>Contact Info</h3>
-        <hr>
+        <hr />
         <div className="contact-item">
           <p>Phone Number</p>
           <p>Email Address</p>
         </div>
-      </div>
     </div>
     <div className="col-md-8 col-md-offset-2">
       <h3>Leave us a message</h3>
@@ -42,14 +41,14 @@ render() {
         <div className="row">
           <div className="col-md-6">
             <div className="form-group">
-              <input type="text" id="name" className="form-control" placeholder="Name" required="required">
+              <input type="text" id="name" className="form-control" placeholder="Name" required="required" />
               <p className="help-block text-danger"></p>
             </div>
           </div>
 
           <div className="col-md-6">
             <div className="form-group">
-              <input type="email" id="email" className="form-control" placeholder="Email" required="required">
+              <input type="email" id="email" className="form-control" placeholder="Email" required="required" />
               <p className="help-block text-danger"></p>
             </div>
           </div>
@@ -64,6 +63,7 @@ render() {
     </div>
   </div>
 </div>
+</div>
 );
-};
-};
+}
+}
