ba7a8e9c1c93fb72a9c0c020b3917a6e61073090
Kayley Bannister <kayley.bannister@gmail.com>
2019-10-27 16:10:39 -0400

    trying to fix bugs

--- oldmariettalandscaping/client/src/components/Nav.js
+++ oldmariettalandscaping/client/src/components/Nav.js
@@ -3,15 +3,12 @@ import React, { Component } from "react";
 export class Nav extends Component {
     render() {
       return (
-  
         <nav id="menu" className="navbar navbar-default navbar-fixed-top">
           <div className="container"> 
-            {/* Brand and toggle get grouped for better mobile display */}
             <div className="navbar-header">
               <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span className="sr-only">Toggle navigation</span> <span className="icon-bar" /> <span className="icon-bar" /> <span className="icon-bar" /> </button>
-              <a className="navbar-brand page-scroll" href="#page-top">Landscaper</a> </div>
-            {/* Collect the nav links, forms, and other content for toggling */}
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <a className="navbar-brand page-scroll" href="#page-top">OLD MARIETTA LANDSCAPING</a>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
               <ul className="nav navbar-nav navbar-right">
                 <li><a href="#about" className="page-scroll">About</a></li>
                 <li><a href="#services" className="page-scroll">Services</a></li>
@@ -21,6 +18,7 @@ export class Nav extends Component {
                 <li><a href="#customer" className="page-scroll">Customer Portal</a></li>
               </ul>
             </div>
+          </div>
           </div>
         </nav>
       );
