509a5321cf44d5cacf3d58f9b8edf585b9f2c26b
Brendan Goudy <brendan.p.goudy@gmail.com>
2019-11-07 15:56:15 -0500

    Page with link to appointment book

---
+++ client/src/components/LoginLanding.js
@@ -0,0 +1,18 @@
+var NewComponent = React.createClass({
+    render: function() {
+      return (
+        <div>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <title />
+          <meta name="description" content />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="stylesheet" href />
+          {/*[if lt IE 7]>
+              <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+          <![endif]*/}
+          <img src="https://www.appointletcdn.com/loader/buttons/2ECC71.png" data-appointlet-organization="old-marietta-landscaping" data-appointlet-service={198737} />
+        </div>
+      );
+    }
+  });
